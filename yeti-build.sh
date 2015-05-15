495b6ea1be3c9070457bb69c4d16470ede3c720f
Adam Markham <mountainraptor@comcast.net>
2015-05-14 20:41:42 -0600

    yeti-build.sh

--- yeti-build.sh
+++ yeti-build.sh
@@ -50,3 +50,38 @@ then
 	exit 1
 fi
 
+APDIR="project-yeti"
+
+if [ ! -d "$APDIR" ];
+then
+	echo "Couldn't find Project Yeti Directory, cloning from githug"
+	git clone https://github.com/mountainraptor/project-yeti.git project-yeti
+else
+	echo "Found Project Yeti directory, fetching and free basing"
+	cd project-yeti
+	git fetch
+	git rebase
+	cd ..
+fi
+
+echo "Updating mac80211.sh for default monitor mode"
+cp -fv project-yeti/openwrt-files/mac80211.sh package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+echo "Updating shadow file"
+cp -fv project-yeti/openwrt-files/shadow package/base-files/files/etc/shadow
+
+echo "Updating rc.local"
+cp -fv project-yeti/openwrt-files/rc.local package/base-files/files/etc/rc.local
+
+echo "Installing secret sauce"
+cp -fv project-yeti/wifi-sniffer/logwifiids.py package/base-files/files/sbin/
+cp -fv project-yeti/wifi-sniffer/logwifiids package/base-files/files/etc/init.d/
+
+echo "Updating make configuration"
+cp -fv project-yeti/openwrt-files/.config .
+
+echo "Patched OpenWRT build!  Making now"
+
+make
+
+
