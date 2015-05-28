f30a87b4d30e0b585afee36275206750fa1f86e9
bbrother <brett.brotherton@gmail.com>
2015-05-27 20:03:50 -0600

    Updated bluetooth to log almost all entries
    -greatly changed how things are logged to match wifi
    -only filter on errors now

--- bluetooth-sniffer/logbtids.py
+++ bluetooth-sniffer/logbtids.py
@@ -3,35 +3,36 @@
 import os, sys, argparse, subprocess, time, sqlite3
 import signal
 
-HASH_MAP_SIZE = 512
-HASH_MAP_STALE_TIME = 15
+HASH_MAP_SIZE = 2048
 p = None
-
-lapDb = None
+STORE_EXTRA_INFO=False
+HASH_ADDRESS=False
 
 class LapEntry(object):
    def __init__(self, epoch, channel, lap, errors, clk100ns, clk1, signal, noise, snr):
       self.epoch = epoch
       self.channel = channel
-      self.lap = lap
+      if (HASH_ADDRESS):
+         self.addr = buffer(md5.new(lap).digest())
+      else:
+         self.addr = buffer(lap)
       self.errors = errors
       self.clk100ns = clk100ns
       self.clk1 = clk1
       self.signal = signal
       self.noise = noise
       self.snr = snr
-      self.count = 1
       self.lastEpoch = epoch
 
    def __eq__(self, other):
       try:
-         return (self.lap == other.lap)
+         return (self.addr == other.addr)#(''.join('{:02x}'.format(ord(x)) for x in str(self.addr)) == ''.join('{:02x}'.format(ord(x)) for x in str(other.addr)))
       except:
          return False
 
    def isNextValid(self, nextLapEntry):
       try:
-         if (self.lap == nextLapEntry.lap and self.clk100ns < nextLapEntry.clk100ns and self.clk1 < nextLapEntry.clk1):
+         if (self.addr == nextLapEntry.addr and self.clk100ns < nextLapEntry.clk100ns and self.clk1 < nextLapEntry.clk1):
             return True
          else:
             return False
@@ -39,31 +40,8 @@ class LapEntry(object):
          print 'Error comparing next entry'
          return False
    
-   def addNewEntry(self, nextLapEntry):
-      if (self.lap != nextLapEntry.lap):
-         print 'Error does not match'
-         return None
-      if self.isNextValid(nextLapEntry) == False:
-         print 'Invalid entry'
-         if (self.errors > nextLapEntry.errors):
-            print 'Using newer entry with less errors'
-            self.count = 1
-            self = nextLapEntry
-            print self
-            print nextLapEntry
-         return None
-      self.count += 1
-      self.lastEpoch = nextLapEntry.epoch
-      self.errors = min(self.errors, nextLapEntry.errors)
-      self.clk1 = max(self.clk1, nextLapEntry.clk1)
-      self.clk100ns = max(self.clk100ns, nextLapEntry.clk100ns)
-      self.snr = max(self.snr, nextLapEntry.snr)
-      self.signal = max(self.signal, nextLapEntry.signal)
-      self.noise = max(self.noise, nextLapEntry.noise)
-      return self.count
-   
    def __repr__(self):
-      return 'LAP %s, errors = %d, ch = %d, count = %d' % (hex(self.lap), self.errors, self.channel, self.count)
+      return 'AddrHash %s, errors = %d, ch = %d, signal = %d' % (''.join('{:02x}'.format(ord(x)) for x in str(self.addr)), self.errors, self.channel, self.snr)
 
 def textToLapEntry(line):
    s = line.replace('= ', '=').split(' ')
@@ -73,7 +51,7 @@ def textToLapEntry(line):
    #TODO try:
    time = int(s[0].split('=')[1])
    channel = int(s[1].split('=')[1])
-   lap = int(s[2].split('=')[1], 16)
+   lap = s[2].split('=')[1]
    errors = int(s[3].split('=')[1])
    clk100ns = int(s[4].split('=')[1])
    clk1 = int(s[5].split('=')[1])
@@ -82,34 +60,46 @@ def textToLapEntry(line):
    snr = int(s[8].split('=')[1])
    return LapEntry(time, channel, lap, errors, clk100ns, clk1, signal, noise, snr)
 
-def flushAllHashMap(dB, hashMap):
-   for i in range(0, len(hashMap)):
-      if (hashMap[i] != None):
-         dB.addEntryIfValid(hashMap[i])
-         hashMap[i] = None
-   dB.commitOutstandingEntries()
+def isValidEntry(entry, previousEntry):
+   if (entry.errors == 0):
+      return True
+   if (previousEntry != None):
+      return True
+   return False
 
-def cleanupHashMap(dB, hashMap):
-   curTime = int(time.time())
-   for i in range(0, len(hashMap)):
-      if (hashMap[i] != None):
-         if (isEntryStale(hashMap[i])):
-            dB.addEntryIfValid(hashMap[i])
-            hashMap[i] = None
-   dB.commitOutstandingEntries()
-
-def isEntryStale(entry, curTime=None):
-   if (curTime == None):
-      curTime = int(time.time())
-   return (entry.lastEpoch + HASH_MAP_STALE_TIME < curTime)
+def updateHashAndCommitValidEntries(hashMap, entry, dB):
+   hashIdx = hashFunction(entry)
+   previousEntry = None
+   valid = False
+   if (hashMap[hashIdx] != None):
+      (previousEntry, previousValid) = hashMap[hashIdx]
+      print '\tPrevious found'
+      #if previous entry in hashMap is a match and not too old we consider
+      #them to be the same
+      if not (previousEntry == entry):
+         previousEntry = None
+         print '\t\tEntries do not match'
+      elif (entry.epoch - previousEntry.epoch) > 15:
+         previousEntry = None
+         print '\t\tPrevious entry stale'
+   
+   if (previousEntry != None or entry.errors == 0):
+      valid = True
+      print '\tValid entry'
+      if (previousEntry != None and previousValid == False):
+         print '\t\tAdding previously invalid entry'
+         dB.addEntry(previousEntry)
+      dB.addEntry(entry)
+      
+   hashMap[hashIdx] = (entry, valid)
 
 def hashFunction(lapEntry):
-   return lapEntry.lap % HASH_MAP_SIZE
+   return hash(lapEntry.addr) % HASH_MAP_SIZE
 
 def preexec_function():
-    # Ignore the SIGINT signal by setting the handler to the standard
-    # signal handler SIG_IGN.
-    signal.signal(signal.SIGINT, signal.SIG_IGN)
+   # Ignore the SIGINT signal by setting the handler to the standard
+   # signal handler SIG_IGN.
+   signal.signal(signal.SIGINT, signal.SIG_IGN)
 
 def runUbertoothRx(dB, maxErrors):
    global p
@@ -117,50 +107,46 @@ def runUbertoothRx(dB, maxErrors):
    p = subprocess.Popen(['unbuffer', 'ubertooth-rx', '-e', str(maxErrors), '-s'], stdout=subprocess.PIPE, preexec_fn = preexec_function)
    while p.poll() is None:
       entry = p.stdout.readline()
+      #print entry
       le = textToLapEntry(entry)
       if (le == None):
          continue
-      cleanupHashMap(dB, hashMap)
-      hashIndex = hashFunction(le)
-      if (hashMap[hashIndex] != None):
-         if (hashMap[hashIndex] == le):
-            hashMap[hashIndex].addNewEntry(le)
-         else:
-            print 'hash collision ' + str(hashMap[hashIndex]) + ' ' + str(le)
-            dB.addSingleEntryIfValid(hashMap[hashIndex])
-            hashMap[hashIndex] = le
-      else:
-         hashMap[hashIndex] = le
-         
-      print hashMap[hashIndex]
-      
-      
-   print 'Flushing all hashmap entries and exiting'
-   flushAllHashMap(dB, hashMap)
+      print le
+      updateHashAndCommitValidEntries(hashMap, le, dB)
+      dB.commitTimer()
+   dB.commitOutstandingEntries()
 
 class lapDb(object):
+   FLUSH_INTERVAL = 10
    def __init__(self, dBFile):
+      self.lastFlush = None
       self.conn = sqlite3.connect(dBFile)
       self.cursor = self.conn.cursor()
       #check to see if table is created
       try:
          self.cursor.execute('SELECT * FROM lapTable LIMIT 1')
       except Exception as e:
-         self.cursor.execute('CREATE TABLE lapTable(addr INT NOT NULL, epoch INT NOT NULL, errors INT NOT NULL, count INT NOT NULL, duration INT NOT NULL, snr INT NOT NULL, extrainfo STRING)')
+         self.cursor.execute('CREATE TABLE lapTable(addrHash BLOB NOT NULL, epoch INT NOT NULL, errors INT NOT NULL, snr INT NOT NULL, extrainfo STRING)')
          self.conn.commit()
       
-   def addSingleEntryIfValid(self, entry):
+   def addSingleEntry(self, entry):
       self.addEntryIfValid(entry)
       self.commitOutstandingEntries()
       
-   def addEntryIfValid(self, entry):
-      if (entry.count > 1 or entry.errors == 0):
-         print 'Adding entry for ' + str(entry)
-         duration = entry.lastEpoch - entry.epoch
-         self.cursor.execute('INSERT INTO lapTable VALUES(?, ?, ?, ?, ?, ?)', (entry.lap, entry.epoch, entry.errors, entry.count, duration, entry.snr))
+   def addEntry(self, entry):
+      self.cursor.execute('INSERT INTO lapTable VALUES(?, ?, ?, ?, ?)', (entry.addr, entry.epoch, entry.errors, entry.snr, None  ))
+      if (self.lastFlush == None):
+         self.lastFlush = time.time()
       
    def commitOutstandingEntries(self):
+      print 'Flushing database entries'
       self.conn.commit()
+      self.lastFlush = None
+      
+   def commitTimer(self):
+      if (self.lastFlush != None):
+         if (time.time() > self.lastFlush + lapDb.FLUSH_INTERVAL):
+            self.commitOutstandingEntries()
 
 def signal_handler(signal, frame):
    print('You pressed Ctrl+C!')
@@ -172,12 +158,21 @@ def signal_handler(signal, frame):
 def main():
    signal.signal(signal.SIGINT, signal_handler)
    parser = argparse.ArgumentParser(description='Log bluetooth LAPs to sqlite dB using ubertooth-rx')
-   parser.add_argument('-d', '--database', help='SQLite dB file to store LAPs in', default='bt-laps.sqlite')
-   parser.add_argument('-e', '--max-errors', help='Max number of errors in decoded packet for ubertooth', default=2)
+   parser.add_argument('-d', '--database-folder', help='SQLite dB file to store data in', default='./')
+   parser.add_argument('-e', '--max-errors', help='Max number of errors in decoded packet for ubertooth', default=3)
    
    config = parser.parse_args()
    
-   dB = lapDb(config.database)
+   num = 0
+   while True:
+      dbpath = '%s/bt-macs_%04d.sqlite' % (config.database_folder, num)
+      num += 1
+      if (not os.path.isfile(dbpath)):
+         break
+      
+   print 'using db ' + dbpath
+      
+   dB = lapDb(dbpath)
    
    runUbertoothRx(dB, config.max_errors)
 
