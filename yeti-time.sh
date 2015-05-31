120159b22a52dfd7226f5f530c14c0b47bd7ce9d
mountainraptor <amarkham@umich.edu>
2015-05-31 10:49:33 -0600

    added script utility to set the time on yeti

---
+++ yeti-time.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+
+read -r -p "Do you want to update your time from time.nist.gov? [y,N] " response
+response=${response,,}
+if [[ $response =~ ^(yes|y)$ ]];
+then
+	sudo ntpdate -s time.nist.gov
+	if [ $? -ne 0 ];
+	then
+	read -r -p "Do you want to update your time from time.nist.gov? [y,N] " response
+	response=${response,,}
+	if [[ ! $response =~ ^(yes|y)$ ]];
+	then
+		echo "Exiting without updating yeti time"
+		exit 1
+	fi
+fi
+
+DATE=$(date)
+
+ssh root@192.168.1.1 "date --set='$DATE'"
+
+
