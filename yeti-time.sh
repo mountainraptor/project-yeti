771b1cf7d52abbf59957cec53d2f8aee00a8e57f
mountainraptor <amarkham@umich.edu>
2015-05-31 11:10:28 -0600

    fixed script to use epoch time and be compatible with OpenWRT

--- yeti-time.sh
+++ yeti-time.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 read -r -p "Do you want to update your time from time.nist.gov? [y,N] " response
 response=${response,,}
 if [[ $response =~ ^(yes|y)$ ]];
@@ -9,17 +7,16 @@ then
 	sudo ntpdate -s time.nist.gov
 	if [ $? -ne 0 ];
 	then
-	read -r -p "Do you want to update your time from time.nist.gov? [y,N] " response
-	response=${response,,}
-	if [[ ! $response =~ ^(yes|y)$ ]];
-	then
-		echo "Exiting without updating yeti time"
-		exit 1
+		read -r -p "Do you want to update your time from time.nist.gov? [y,N] " response
+		response=${response,,}
+		if [[ ! $response =~ ^(yes|y)$ ]];
+		then
+			echo "Exiting without updating yeti time"
+			exit 1
+		fi
 	fi
 fi
 
-DATE=$(date)
+DATE=$(date +%s)
 
-ssh root@192.168.1.1 "date --set='$DATE'"
-
-
+ssh root@192.168.1.1 'date --set="@'$DATE'"'
